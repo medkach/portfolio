800991bea0d93112be21ce8409965af409de1a1a
medkach <mohamed.kachout1@gmail.com>
2024-07-21 09:38:11 +0100

    Initial commit portfolio

---
+++ src/app/app.module.ts
@@ -0,0 +1,87 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from "@angular/material/list";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadStudentsComponent } from './load-students/load-students.component';
+import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import { StudentsComponent } from './students/students.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkVirtualForOf,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollViewport
+} from "@angular/cdk/scrolling";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import { PaymentsComponent } from './payments/payments.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { TechnicalSkillsComponent } from './technical-skills/technical-skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminTemplateComponent,
+    ProfileComponent,
+    HomeComponent,
+    LoadStudentsComponent,
+    LoadPaymentsComponent,
+    StudentsComponent,
+    PaymentsComponent,
+    DashboardComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    EducationComponent,
+    TechnicalSkillsComponent,
+    WorkExperienceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatDrawerContainer,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf,
+    CdkVirtualScrollableElement,
+    MatProgressBarModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
